6d533d12a045f33291b231bf03087f406c0cf3bc
PetrescuRobert <petrescu.robert69@gmail.com>
2025-12-21 11:53:13 +0200

    Course Controller test

---
+++ apps/smart-academy/src/course/presenters/http/course.controller.spec.ts
@@ -0,0 +1,61 @@
+import { CourseService } from '../../application/course.service';
+import { Course } from '../../domain/entities/course.entity';
+import { CourseId } from '../../domain/value-objects/course-id.vo';
+import { CourseController } from './course.controller';
+import { CourseDto } from './dtos/course.dto';
+import { CreateCourseDto } from './dtos/create-course.dto';
+
+describe('Courses controller - tests suite', () => {
+  let coursesController: CourseController;
+  let coursesService: CourseService;
+
+  beforeEach(async () => {
+    coursesService = {
+      create: jest.fn(),
+    } as unknown as jest.Mocked<CourseService>;
+    coursesController = new CourseController(coursesService);
+  });
+
+  it('recieves the create course request and call the service with the correct parameter', async () => {
+    // ARRANGE
+    const body: CreateCourseDto = {
+      title: 'My first controller test',
+      description: 'My description for the first test',
+      active: false,
+    };
+
+    const savedCourse: Course = new Course(
+      new CourseId('uuid'),
+      'My first controller test',
+      'My description for the first test',
+      false
+    );
+
+    const responseDto: CourseDto = {
+      id: 'uuid',
+      title: 'My first controller test',
+      description: 'My description for the first test',
+      isActive: false,
+    };
+
+    jest.spyOn(coursesService, 'create').mockResolvedValue(savedCourse);
+
+    jest.spyOn(CourseDto, 'fromEntity').mockReturnValue(responseDto);
+
+    // ACT
+    const result = await coursesController.createCourse(body);
+
+    // ASSERT
+    expect(coursesService.create).toHaveBeenCalledTimes(1);
+    expect(coursesService.create).toHaveBeenCalledWith({
+      title: body.title,
+      description: body.description,
+      active: body.active,
+    });
+
+    expect(CourseDto.fromEntity).toHaveBeenCalledTimes(1);
+    expect(CourseDto.fromEntity).toHaveBeenCalledWith(savedCourse);
+
+    expect(result).toEqual<CourseDto>(responseDto);
+  });
+});
